c772c5fd4c4f19d07214a430b6689e7dae656f78
Devel0per Marley <49081673+marloss@users.noreply.github.com>
2024-03-12 17:05:05 +0100

    Added basic following with navmesh plus to the Enemy.cs

--- Gila/Assets/Scripts/Enemy.cs
+++ Gila/Assets/Scripts/Enemy.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     private int health;
+    private NavMeshAgent _agent;
+    private GameObject player;
+    
+    
     private void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        _agent.SetDestination(player.transform.position);
     }
 
     private void DepleteHealth()
