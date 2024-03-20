de86f3f2376fe7ca3b07ebb38d7bc079d49f9dd7
Devel0per Marley <49081673+marloss@users.noreply.github.com>
2024-03-20 23:30:26 +0100

    Added enemy detection cone. Added crosshair. New file management system.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Bullet : MonoBehaviour
+    {
+        private float _speed = 15f * 100f;
+        private Rigidbody2D _bulletRigidbody;
+        private int damage;
+
+        private void Start()
+        {
+            _bulletRigidbody = GetComponent<Rigidbody2D>();
+            Destroy(this,5f);
+        }
+
+        private void FixedUpdate()
+        {
+            _bulletRigidbody.velocity = ((Vector2)transform.up) * (_speed * Time.deltaTime);
+        }
+
+        public int GetDamage() => damage;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player") is false) Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour, IEntity
+{
+    public Entity origin;
+
+    //
+    private int _health;
+    private NavMeshAgent _agent;
+    private GameObject _player;
+
+    private Transform _spriteTransform;
+    //
+
+    private bool _isDetected = false;
+
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+        _agent.speed = origin.speed;
+        _health = origin.health;
+        _spriteTransform = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        if (_isDetected is false) return;
+        _agent.SetDestination(_player.transform.position);
+        ObjectSpinner.SpinObject(transform, _spriteTransform,
+            _agent.nextPosition);
+    }
+
+    private void LateUpdate()
+    {
+        if (_isDetected) return;
+        if (IsPlayerWithinRadius() is false) return;
+        if (IsInConeOfVision() is false) return;
+        _isDetected = _isDetected is false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Bullet")) return;
+        var bullet = other.GetComponent<Bullet>();
+        Damage(1);
+    }
+
+    public void Heal(int addition)
+    {
+        _health += addition;
+    }
+
+    public void Damage(int damage)
+    {
+        _health -= damage;
+        if (_isDetected) return;
+        _isDetected = _isDetected is false;
+    }
+
+    private bool IsInConeOfVision() =>
+        PlayerEnemyAngle() is >= (float)-Entity.VisionConeDegree and <= (float)Entity.VisionConeDegree;
+
+    private float PlayerEnemyAngle() =>
+        Vector2.SignedAngle(
+            ObjectSpinner.DirectionVector(
+                GetPositionVector2(),
+                GetPositionVector2() + Vector2.up),
+            PlayerDirection());
+
+    private bool IsPlayerWithinRadius() => GetPlayerDistance() < origin.distance;
+
+    private float GetPlayerDistance() => Vector2.Distance(_player.transform.position, transform.position);
+
+    private Vector2 PlayerDirection() => ObjectSpinner.DirectionVector(GetPositionVector2(), PlayerPosition());
+
+    private Vector2 PlayerPosition() => _player.transform.position;
+    private Vector2 GetPositionVector2() => transform.position;
+}
