59f6209b6859dca011a2da8a7652dd26995b2571
Devel0per Marley <49081673+marloss@users.noreply.github.com>
2024-04-04 23:35:16 +0200

    Added four directional player sprite sheet (without color) Added four directional basic enemy sprite sheet (without color) Added player blend tree Added revolver model

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
@@ -9,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _input;
     public float movementSpeed;
     private Rigidbody2D _playerPhysicsRigidbody;
+    private Animator _animation;
 
     private void OnEnable()
     {
@@ -23,12 +25,15 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         _playerPhysicsRigidbody = GetComponent<Rigidbody2D>();
+        _animation = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         FetchInput();
         _playerPhysicsRigidbody.velocity = _input * (movementSpeed * (1 / Time.deltaTime) * Time.deltaTime);
+        _animation.SetFloat("horizontal", _input.x);
+        _animation.SetFloat("vertical", _input.y);
     }
 
 
